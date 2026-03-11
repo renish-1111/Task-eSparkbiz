9b3a0fbcee8d25683c38357e4145f81a1ff55b6f
renish-1111 <ponkiyarenish@gmail.com>
2026-03-11 17:46:30 +0530

    near complete

--- NodeJs/Pagination/server.js
+++ NodeJs/Pagination/server.js
@@ -13,37 +13,36 @@ const port = 3000
 
 app.set("view engine", "ejs")
 
-app.get('/',async (req, res) => {
+app.get('/', async (req, res) => {
 
     let page = parseInt(req.query.page) || 1;
+    let offset = parseInt(req.query.offset) || 100;
+    let sorting = req.query.sorting || "id,asc";
+    let search = req.query.search || "";
 
-   
 
-    let onetime = 100
-
-    let limit = (page - 1) * onetime 
-    let offset = limit + onetime
-    console.log({"limit":limit,"offset":offset});
+    let limit = (page - 1) * offset
 
     try {
         var [results, fields] = await connection.query(
             'SELECT COUNT(id) as count FROM student'
         );
+        let recode = results[0].count;
+        let searchQuary = ""
+        if (search || sorting) {
+            let sortparameter = sorting.split(",")
+            searchQuary = `WHERE name LIKE '%${search}%' OR phone LIKE '%${search}%' OR dob LIKE '%${search}%' ORDER BY ${sortparameter[0]} ${sortparameter[1]}`
+        }
 
-        let recode = results[0].count; 
 
         var [results, fields] = await connection.query(
-            `SELECT id,name,email,phone,address,city,dob,gender FROM student LIMIT ${limit}, ${onetime}`
+            `SELECT id,name,email,phone,address,city,dob,gender FROM student ${searchQuary} LIMIT ?, ?`, [limit, offset]
         );
-        
-        res.render("index.ejs",{"data":results,"page":page,"total":Math.ceil(recode/onetime)});
-        
+
+        res.render("index.ejs", { "data": results, "page": page, "total": Math.ceil(recode / offset) });
+
     } catch (err) {
         console.log(err);
     }
-
-    
-
-
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
